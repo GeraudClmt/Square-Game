322febf20e879cb314fa73070b2a256199f652c2
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-10-15 14:29:48 +0200

    Création de la classe InputOuput, pour la gestion d'affichage

--- src/fr/squaregame/components/InputOutput.java
+++ src/fr/squaregame/components/InputOutput.java
@@ -1,4 +1,20 @@
 package fr.squaregame.components;
 
+import java.util.Scanner;
+
 public class InputOutput {
+    private final Scanner scanner;
+
+    public InputOutput(){
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int getInputInt(String message){
+        printMessage(message);
+        return scanner.nextInt();
+    }
+
+    public void printMessage(String message){
+        System.out.println(message);
+    }
 }
