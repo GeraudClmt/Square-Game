d46fdecd3c9cd83e5f6684fb01a0c19f45f15f18
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-10-20 15:25:31 +0200

    Re factorisation des méthodes, héritage sur les jeux

--- src/fr/squaregame/games/Game.java
+++ src/fr/squaregame/games/Game.java
@@ -1,11 +1,32 @@
 package fr.squaregame.games;
 
-import fr.squaregame.components.Board;
+import fr.squaregame.components.*;
+import fr.squaregame.exceptions.BoardIsFull;
+import fr.squaregame.exceptions.PlayerWin;
 
 public abstract class Game {
+    protected Player player1;
+    protected Player player2;
     protected Board board;
     protected int width;
     protected int height;
+    protected int countTurnPlayed;
+    protected int winningLength;
+
+    public void setOwner(int x, int y, Player player){
+        board.setCell(x, y, new Cell(player.getRepresentation()));
+        countTurnPlayed++;
+    }
+
+    public void isOver(int row, int col)throws PlayerWin, BoardIsFull {
+        if(countAlignement(row, col) >= winningLength){
+            throw new PlayerWin(board.getCell(row, col).getRepresentation());
+        }
+
+        if(countTurnPlayed == width * height){
+            throw new BoardIsFull("Egalité, le plateau est complet");
+        }
+    }
 
     public int countAlignement(int row, int col ){
         int maxOccurence = 0;
@@ -79,5 +100,59 @@ public abstract class Game {
 
 
         return maxOccurence;
+    }
+
+    public abstract void playerTurn(Player player, InteractionUtilisateur interactUser, View view)throws PlayerWin, BoardIsFull;
+
+    public void play(InteractionUtilisateur interactUser, View view){
+        view.printMessage("Début du jeux");
+        boolean isPlay = true;
+        String signPlayer1;
+        String signPlayer2;
+
+        boolean isHumanPlayer1 = interactUser.isPositifResponse("Le premier joueur est un humain ? Y / N");
+        if(isHumanPlayer1){
+            signPlayer1 = interactUser.getSign();
+            player1 = new HumanPlayer(signPlayer1);
+        }else {
+            signPlayer1 = "X";
+            player1 = new ArtificialPlayer(signPlayer1);
+        }
+
+        if(signPlayer1.equals("X")){
+            signPlayer2 = "O";
+        }else{
+            signPlayer2 = "X";
+        }
+
+        boolean isHumanPlayer2 = interactUser.isPositifResponse("Le deuxième joueur est un humain ? Y / N");
+        if(isHumanPlayer2){
+            player2 = new HumanPlayer(signPlayer2);
+        }else{
+            player2 = new ArtificialPlayer(signPlayer2);
+        }
+
+        view.printMessage("Joueur 1 : " + signPlayer1 + " et Joueur 2 : " + signPlayer2);
+
+        while(isPlay){
+            try{
+                view.display(board);
+                view.printMessage("\nAu tour du joueur 1");
+                playerTurn(player1, interactUser, view);
+                view.printMessage("\nAu tour du joueur 2");
+                playerTurn(player2, interactUser, view);
+
+            } catch (PlayerWin e) {
+                if(player1.getRepresentation().equals(e.getMessage().trim())){
+                    view.printMessage("Le joueur 1 gagne");
+                }else{
+                    view.printMessage("Le joueur 2 gagne");
+                }
+                isPlay = false;
+            } catch (BoardIsFull e){
+                view.printMessage(e.getMessage());
+                isPlay =  false;
+            }
+        }
     }
 }
--- src/fr/squaregame/games/gomoku/Gomoku.java
+++ src/fr/squaregame/games/gomoku/Gomoku.java
@@ -6,32 +6,12 @@ import fr.squaregame.exceptions.PlayerWin;
 import fr.squaregame.games.Game;
 
 public class Gomoku extends Game {
-    private Player player1;
-    private Player player2;
-    private int countTurnPlayed;
-    private final int winningLength;
-
     public Gomoku(){
         super.width = 15;
         super.height = 15;
-        super.board = new Board(height, width);
-        winningLength = 5;
-        countTurnPlayed = 0;
-    }
-
-    public void setOwner(int x, int y, Player player){
-        board.setCell(x, y, new Cell(player.getRepresentation()));
-        countTurnPlayed++;
-    }
-
-    public void isOver(int row, int col)throws PlayerWin, BoardIsFull {
-        if(countAlignement(row, col) >= winningLength){
-            throw new PlayerWin(board.getCell(row, col).getRepresentation());
-        }
-
-        if(countTurnPlayed == width * height){
-            throw new BoardIsFull("Egalité, le plateau est complet");
-        }
+        super.board = new Board(height, width, " ");
+        super.winningLength = 5;
+        super.countTurnPlayed = 0;
     }
 
     public void playerTurn(Player player, InteractionUtilisateur interactUser, View view)throws PlayerWin, BoardIsFull{
@@ -47,57 +27,5 @@ public class Gomoku extends Game {
         setOwner(coordinate[0], coordinate[1], player);
         view.display(board);
         isOver(coordinate[0], coordinate[1]);
-    }
-
-    public void play(InteractionUtilisateur interactUser, View view){
-        view.printMessage("Début du jeux");
-        boolean isPlay = true;
-        String signPlayer1;
-        String signPlayer2;
-
-        boolean isHumanPlayer1 = interactUser.isPositifResponse("Le premier joueur est un humain ? Y / N");
-        if(isHumanPlayer1){
-            signPlayer1 = interactUser.getSign();
-            player1 = new HumanPlayer(signPlayer1);
-        }else {
-            signPlayer1 = "X";
-            player1 = new ArtificialPlayer(signPlayer1);
-        }
-
-        if(signPlayer1.equals("X")){
-            signPlayer2 = "O";
-        }else{
-            signPlayer2 = "X";
-        }
-
-        boolean isHumanPlayer2 = interactUser.isPositifResponse("Le deuxième joueur est un humain ? Y / N");
-        if(isHumanPlayer2){
-            player2 = new HumanPlayer(signPlayer2);
-        }else{
-            player2 = new ArtificialPlayer(signPlayer2);
-        }
-
-        view.printMessage("Joueur 1 : " + signPlayer1 + " et Joueur 2 : " + signPlayer2);
-
-        while(isPlay){
-            try{
-                view.display(board);
-                view.printMessage("\nAu tour du joueur 1");
-                playerTurn(player1, interactUser, view);
-                view.printMessage("\nAu tour du joueur 2");
-                playerTurn(player2, interactUser, view);
-
-            } catch (PlayerWin e) {
-                if(player1.getRepresentation().equals(e.getMessage().trim())){
-                    view.printMessage("Le joueur 1 gagne");
-                }else{
-                    view.printMessage("Le joueur 2 gagne");
-                }
-                isPlay = false;
-            } catch (BoardIsFull e){
-                view.printMessage(e.getMessage());
-                isPlay =  false;
-            }
-        }
     }
 }
