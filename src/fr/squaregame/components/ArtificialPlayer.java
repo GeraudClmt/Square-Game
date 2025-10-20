2093b47aac46f6d24a551beb5427980a45ae38c8
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-10-20 14:31:15 +0200

    Surcharge de la classe ArtificialPlayer

--- src/fr/squaregame/components/ArtificialPlayer.java
+++ src/fr/squaregame/components/ArtificialPlayer.java
@@ -1,8 +1,34 @@
 package fr.squaregame.components;
 
-public class ArtificialPlayer extends Player{
+import java.util.Random;
+
+public class ArtificialPlayer extends Player {
     public ArtificialPlayer(String representation) {
         super(representation);
-        this.type = Type.MACHINE;
+    }
+
+    @Override
+    public int[] getCoordinates(InteractionUtilisateur interactUser, View view, Board board) throws ArrayIndexOutOfBoundsException {
+        Random rand = new Random();
+        int row = rand.nextInt(0, board.getHeight());
+        int col = rand.nextInt(0, board.getWidth());
+
+        if (board.getCell(row, col).getRepresentation().equals("   ")) {
+            return new int[]{row, col};
+        }
+
+        return getCoordinates(interactUser, view, board);
+    }
+
+    @Override
+    public int getCol(InteractionUtilisateur interactUser, View view, Board board) throws ArrayIndexOutOfBoundsException {
+        Random rand = new Random();
+        int col = rand.nextInt(0, board.getWidth());
+
+        if (board.getCell(0, col).getRepresentation().equals("   ")) {
+            return col;
+        }
+
+        return getCol(interactUser, view, board);
     }
 }
