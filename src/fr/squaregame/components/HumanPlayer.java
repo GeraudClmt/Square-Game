c0584b18789ab6e5cbc907aff82facb8f9d2e23f
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-10-20 14:31:28 +0200

    Surcharge de la classe HumanPlayer

--- src/fr/squaregame/components/HumanPlayer.java
+++ src/fr/squaregame/components/HumanPlayer.java
@@ -4,6 +4,30 @@ public class HumanPlayer extends Player{
 
     public HumanPlayer(String representation) {
         super(representation);
-        this.type = Type.HUMAIN;
+    }
+
+    @Override
+    public int[] getCoordinates(InteractionUtilisateur interactUser, View view, Board board) throws ArrayIndexOutOfBoundsException {
+        int line = interactUser.getInputInt("Entrez une ligne") - 1;
+        int column = interactUser.getInputInt("Entrez une colonne") - 1;
+
+        if(!board.getCell(line, column).getRepresentation().equals("   ")){
+            view.printMessage("Case déja prise\n");
+            return getCoordinates(interactUser, view, board);
+        }else{
+            return new int[]{line, column};
+        }
+    }
+
+    @Override
+    public int getCol(InteractionUtilisateur interactUser, View view, Board board) throws ArrayIndexOutOfBoundsException {
+        int column = interactUser.getInputInt("Entrez une colonne") - 1;
+
+        if(!board.getCell(0, column).getRepresentation().equals("   ")){
+            view.printMessage("Colonne pleine\n");
+            return getCol(interactUser, view, board);
+        }else{
+            return column;
+        }
     }
 }
