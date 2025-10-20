59e19e985a585d194334be1004f61b4adeef7f32
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-10-20 14:36:32 +0200

    Ajout d'une méthode display pour affiche d'un Board dans la classe View

--- src/fr/squaregame/components/View.java
+++ src/fr/squaregame/components/View.java
@@ -4,4 +4,22 @@ public class View {
     public void printMessage(String message){
         System.out.println(message);
     }
+
+    public void display(Board board){
+        int width = board.getWidth();
+        int height = board.getHeight();
+        StringBuilder sortie = new StringBuilder();
+
+        for(int i = 0; i < height; i++){
+            sortie.append("\n").append("-".repeat(width * 4+1)).append("\n");
+            for(int j = 0; j < width; j++){
+                sortie.append("|").append(board.getCell(i, j).getRepresentation());
+                if(j == width - 1){
+                    sortie.append("|");
+                }
+            }
+        }
+        sortie.append("\n").append("-".repeat(width * 4+1));
+        printMessage(sortie.toString());
+    }
 }
