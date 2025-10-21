52679d742f69f8b1d813758ca6242765d2e9d177
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-10-21 14:20:29 +0200

    Ajout toString dans la classe Board

--- src/fr/squaregame/components/Board.java
+++ src/fr/squaregame/components/Board.java
@@ -5,12 +5,31 @@ public class Board {
 
     public Board(int height, int width, String emptyCell){
         cells = new Cell[height][width];
-
         for(int i = 0; i < height; i++){
             for(int j = 0; j < width; j++){
                 cells[i][j] = new Cell(emptyCell);
             }
         }
+    }
+
+    @Override
+    public String toString() {
+        int width = this.getWidth();
+        int height = this.getHeight();
+        StringBuilder sortie = new StringBuilder();
+
+        for(int i = 0; i < height; i++){
+            sortie.append("\n").append("-".repeat(width * 4+1)).append("\n");
+            for(int j = 0; j < width; j++){
+                sortie.append("|").append(this.getCell(i, j).getRepresentation());
+                if(j == width - 1){
+                    sortie.append("|");
+                }
+            }
+        }
+        sortie.append("\n").append("-".repeat(width * 4+1));
+
+        return sortie.toString();
     }
 
     public Cell getCell(int row, int col){
@@ -31,4 +50,5 @@ public class Board {
     public boolean isEmptyCell(int row, int col){
         return cells[row][col].isEmpty();
     }
+
 }
