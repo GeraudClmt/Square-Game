282b95d7067e858bf8ee85945ed1125654df07fa
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-10-21 14:22:19 +0200

    Utilisation du toString de Board pour l'afficher avec la classe View

--- src/fr/squaregame/components/View.java
+++ src/fr/squaregame/components/View.java
@@ -6,20 +6,6 @@ public class View {
     }
 
     public void display(Board board){
-        int width = board.getWidth();
-        int height = board.getHeight();
-        StringBuilder sortie = new StringBuilder();
-
-        for(int i = 0; i < height; i++){
-            sortie.append("\n").append("-".repeat(width * 4+1)).append("\n");
-            for(int j = 0; j < width; j++){
-                sortie.append("|").append(board.getCell(i, j).getRepresentation());
-                if(j == width - 1){
-                    sortie.append("|");
-                }
-            }
-        }
-        sortie.append("\n").append("-".repeat(width * 4+1));
-        printMessage(sortie.toString());
+        printMessage(board.toString());
     }
 }
