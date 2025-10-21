4f7667e35ba5fd26afbab542101c90ff329052d4
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-10-21 14:22:49 +0200

    Ajout tests des positions sur le jeu TicTacToe

--- tests/TicTacToeTest.java
+++ tests/TicTacToeTest.java
@@ -1,78 +1,175 @@
 import fr.squaregame.components.Board;
 import fr.squaregame.components.HumanPlayer;
 import fr.squaregame.components.Player;
+import fr.squaregame.components.View;
 import fr.squaregame.exceptions.BoardIsFull;
 import fr.squaregame.exceptions.PlayerWin;
 import fr.squaregame.games.tictactoe.TicTacToe;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TicTacToeTest {
 
-    @Test
-    public void testBoardIsFull() {
-        Player player1 = new HumanPlayer("X");
-        Player player2 = new HumanPlayer("O");
-        TicTacToe game = new TicTacToe();
+    @Nested
+    @DisplayName("Tests des conditions de victoires")
+    class ConditionsDeVictoires{
+        @Test
+        @DisplayName("Test si le board est plein")
+        public void testBoardIsFull() {
+            Player player1 = new HumanPlayer("X");
+            Player player2 = new HumanPlayer("O");
+            TicTacToe game = new TicTacToe();
 
-        game.setOwner(0,0, player1);
-        game.setOwner(0,1, player1);
-        game.setOwner(0,2, player2);
+            game.setOwner(0,0, player1);
+            game.setOwner(0,1, player1);
+            game.setOwner(0,2, player2);
 
-        game.setOwner(1,0, player2);
-        game.setOwner(1,1, player2);
-        game.setOwner(1,2, player1);
+            game.setOwner(1,0, player2);
+            game.setOwner(1,1, player2);
+            game.setOwner(1,2, player1);
 
-        game.setOwner(2,0, player1);
-        game.setOwner(2,1, player2);
-        game.setOwner(2,2, player1);
+            game.setOwner(2,0, player1);
+            game.setOwner(2,1, player2);
+            game.setOwner(2,2, player1);
 
-        assertThrows(BoardIsFull.class, () -> {
-            game.isOver(2,2);
-        });
+            assertThrows(BoardIsFull.class, () -> {
+                game.isOver(2,2);
+            });
+        }
+
+        @Test
+        @DisplayName("Test si joueur gagne sur la premiere ligne")
+        public void testPlayerWinLine1() {
+            Player player1 = new HumanPlayer("X");
+            Player player2 = new HumanPlayer("O");
+            TicTacToe game = new TicTacToe();
+
+            game.setOwner(0,0, player1);
+            game.setOwner(0,1, player1);
+            game.setOwner(0,2, player1);
+
+            assertThrows(PlayerWin.class, () -> {
+                game.isOver(0,2);
+            });
+        }
+        @Test
+        @DisplayName("Test si joueur gagne sur la deuxième ligne")
+        public void testPlayerWinLine2() {
+            Player player1 = new HumanPlayer("X");
+            Player player2 = new HumanPlayer("O");
+            TicTacToe game = new TicTacToe();
+
+            game.setOwner(1,0, player1);
+            game.setOwner(1,1, player1);
+            game.setOwner(1,2, player1);
+
+            assertThrows(PlayerWin.class, () -> {
+                game.isOver(1,2);
+            });
+        }
+        @Test
+        @DisplayName("Test si joueur gagne sur la premiere colonne")
+        public void testPlayerWinColum1() {
+            Player player1 = new HumanPlayer("X");
+            Player player2 = new HumanPlayer("O");
+            TicTacToe game = new TicTacToe();
+
+            game.setOwner(0,1, player1);
+            game.setOwner(1,1, player1);
+            game.setOwner(2,1, player1);
+
+            assertThrows(PlayerWin.class, () -> {
+                game.isOver(2,1);
+            });
+        }
     }
 
-    @Test
-    public void testPlayerWinLine1() {
-        Player player1 = new HumanPlayer("X");
-        Player player2 = new HumanPlayer("O");
-        TicTacToe game = new TicTacToe();
+    @Nested
+    @DisplayName("Tests des positions du board de TictacToe")
+    class TestsPositionsDuBoardTicTacToe{
+        @Test
+        @DisplayName("Test position 0 0")
+        public void testPosition00(){
+            Player player = new HumanPlayer("X");
+            TicTacToe tictactoe = new TicTacToe();
+            tictactoe.setOwner(0, 0, player);
 
-        game.setOwner(0,0, player1);
-        game.setOwner(0,1, player1);
-        game.setOwner(0,2, player1);
+            assertEquals("\n-------------\n| X |   |   |\n-------------\n|   |   |   |\n-------------\n|   |   |   |\n-------------", tictactoe.getBoardToString());
+        }
+        @Test
+        @DisplayName("Test position 0 1")
+        public void testPosition01(){
+            Player player = new HumanPlayer("X");
+            TicTacToe tictactoe = new TicTacToe();
+            tictactoe.setOwner(0, 1, player);
 
-        assertThrows(PlayerWin.class, () -> {
-            game.isOver(0,2);
-        });
-    }
-    @Test
-    public void testPlayerWinLine2() {
-        Player player1 = new HumanPlayer("X");
-        Player player2 = new HumanPlayer("O");
-        TicTacToe game = new TicTacToe();
+            assertEquals("\n-------------\n|   | X |   |\n-------------\n|   |   |   |\n-------------\n|   |   |   |\n-------------", tictactoe.getBoardToString());
+        }
+        @Test
+        @DisplayName("Test position 0 2")
+        public void testPosition02(){
+            Player player = new HumanPlayer("X");
+            TicTacToe tictactoe = new TicTacToe();
+            tictactoe.setOwner(0, 2, player);
 
-        game.setOwner(1,0, player1);
-        game.setOwner(1,1, player1);
-        game.setOwner(1,2, player1);
+            assertEquals("\n-------------\n|   |   | X |\n-------------\n|   |   |   |\n-------------\n|   |   |   |\n-------------", tictactoe.getBoardToString());
+        }
+        @Test
+        @DisplayName("Test position 1 0")
+        public void testPosition10(){
+            Player player = new HumanPlayer("X");
+            TicTacToe tictactoe = new TicTacToe();
+            tictactoe.setOwner(1, 0, player);
 
-        assertThrows(PlayerWin.class, () -> {
-            game.isOver(1,2);
-        });
-    }
-    @Test
-    public void testPlayerWinColum1() {
-        Player player1 = new HumanPlayer("X");
-        Player player2 = new HumanPlayer("O");
-        TicTacToe game = new TicTacToe();
+            assertEquals("\n-------------\n|   |   |   |\n-------------\n| X |   |   |\n-------------\n|   |   |   |\n-------------", tictactoe.getBoardToString());
+        }
+        @Test
+        @DisplayName("Test position 1 1")
+        public void testPosition11(){
+            Player player = new HumanPlayer("X");
+            TicTacToe tictactoe = new TicTacToe();
+            tictactoe.setOwner(1, 1, player);
 
-        game.setOwner(0,1, player1);
-        game.setOwner(1,1, player1);
-        game.setOwner(2,1, player1);
+            assertEquals("\n-------------\n|   |   |   |\n-------------\n|   | X |   |\n-------------\n|   |   |   |\n-------------", tictactoe.getBoardToString());
+        }
+        @Test
+        @DisplayName("Test position 1 2")
+        public void testPosition12(){
+            Player player = new HumanPlayer("X");
+            TicTacToe tictactoe = new TicTacToe();
+            tictactoe.setOwner(1, 2, player);
 
-        assertThrows(PlayerWin.class, () -> {
-            game.isOver(2,1);
-        });
+            assertEquals("\n-------------\n|   |   |   |\n-------------\n|   |   | X |\n-------------\n|   |   |   |\n-------------", tictactoe.getBoardToString());
+        }
+        @Test
+        @DisplayName("Test position 2 0")
+        public void testPosition20(){
+            Player player = new HumanPlayer("X");
+            TicTacToe tictactoe = new TicTacToe();
+            tictactoe.setOwner(2, 0, player);
+
+            assertEquals("\n-------------\n|   |   |   |\n-------------\n|   |   |   |\n-------------\n| X |   |   |\n-------------", tictactoe.getBoardToString());
+        }
+        @Test
+        @DisplayName("Test position 2 1")
+        public void testPosition21(){
+            Player player = new HumanPlayer("X");
+            TicTacToe tictactoe = new TicTacToe();
+            tictactoe.setOwner(2, 1, player);
+
+            assertEquals("\n-------------\n|   |   |   |\n-------------\n|   |   |   |\n-------------\n|   | X |   |\n-------------", tictactoe.getBoardToString());
+        }
+        @Test
+        @DisplayName("Test position 2 2")
+        public void testPosition22(){
+            Player player = new HumanPlayer("X");
+            TicTacToe tictactoe = new TicTacToe();
+            tictactoe.setOwner(2, 2, player);
+
+            assertEquals("\n-------------\n|   |   |   |\n-------------\n|   |   |   |\n-------------\n|   |   | X |\n-------------", tictactoe.getBoardToString());
+        }
     }
 }
